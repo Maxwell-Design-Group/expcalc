03ae415775f05f449b4ebb244a4541cf2ebca4e1
manoj <manojkumar.choubey@primeroedge.com>
2022-07-07 14:49:57 +0530

    suggested code

---
+++ index.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var port = 4200;
+var cors = require('cors');
+
+
+//Mongoose connection with mongodb
+//mongoose.Promise = require('bluebird');
+mongoose.connect('mongodb://localhost:27017/aramarkDB')
+    .then(() => {
+        console.log('Start');
+    })
+    .catch((err) => {
+        console.error('App starting error:', err.stack);
+        process.exit(1);
+    });
+
+// Required aplication specific custom router module
+var clientdetailRouter = require('./src/routes/clientdetailRouter');
+
+// Required aplication specific custom router module
+var contracttypelistRouter = require('./src/routes/contracttypelistRouter');
+
+// Required aplication specific custom router module
+var industrytypeRouter = require('./src/routes/industrytypeRouter');
+
+// Required aplication specific custom router module
+var winthemedetailRouter = require('./src/routes/winthemedetailRouter');
+
+// Required aplication specific custom router module
+//var winthemeRouter = require('./src/routes/winthemeRouter');
+
+// Use middlewares to set view engine and post json data to the server
+app.use(express.static('public'));
+app.use(cors());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+
+app.use('/clientdetails', clientdetailRouter);
+
+app.use('/contracttypelists', contracttypelistRouter);
+
+app.use('/industrytypes', industrytypeRouter);
+
+app.use('/winthemedetails', winthemedetailRouter);
+
+//app.use('/wintheme', winthemeRouter);
+
+
+// Start the server
+app.listen(port, function() {
+    console.log('Server is running on Port: ', port);
+});
+
+app.use(express.static(path.join(__dirname, '/client/build')));
+
+app.get('*', function(req, res){
+     res.sendFile(path.join(__dirname,'/client/build/index.html'));
+});
+
+app.listen(3000);
+
