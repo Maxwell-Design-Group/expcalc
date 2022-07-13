6587e1e5f112a7d877633a99d6e2dd6be2a6c75f
manoj <manojkumar.choubey@primeroedge.com>
2022-07-14 05:23:57 +0530

    added rout without master data

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const PORT = process.env.PORT || 3000
 app.use(express.json())
 
 
- //require('./src/routes/masterdataRoute')(app);
+ require('./src/routes/masterdataRoute')(app);
 
  require('./src/routes/winthemeRoute')(app);
 
