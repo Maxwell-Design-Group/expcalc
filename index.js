c5d86e260ad43eabce46c6b09f7f0d4df5af6658
manoj <manojkumar.choubey@primeroedge.com>
2022-07-13 14:34:06 +0530

    changes in index js

--- index.js
+++ index.js
@@ -1,61 +1,89 @@
-const path = require("path");
-const express = require("express")
-const mongoose = require("mongoose")
+const express = require('express');
+const app = express();
+const path = require('path');
+
+
+var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
-const db = require("./src/db/db")
-//const header_middleware = require("./src/middlewares/header")
-
-
-//const userRoutes = require("./src/Routes/user");
-
-
+var port = process.env.PORT || 4200;
 var cors = require('cors');
 
 
-const app = express()
+// //Mongoose connection with mongodb
+mongoose.Promise = require('bluebird');
+//mongoose.connect('mongodb://localhost:27017/aramarkDB');
+// mongoose.connect(encodeURI('mongodb+srv://dmaxwell:ZiscohFcN6FkC5zb@cluster0.bhz6k.mongodb.net/test/aramarkDB'));
+//     .then(() => {
+//         console.log('Start');
+//     })
+//     .catch((err) => {
+//         console.error('App starting error:', err.stack);
+//         process.exit(1);
+//     });
+// srv style not supporing
+ //mongoose.connect('mongodb+srv://dmaxwell:ZiscohFcN6FkC5zb@cluster0.bhz6k.mongodb.net/test/aramarkDB');
+   
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// }, function(err, db) {
+//   if (err) {
+//     console.log('mongo db error  ', err);
+//   }
+// });
+mongoose.connect('mongodb+srv://dmaxwell:ZiscohFcN6FkC5zb@cluster0.bhz6k.mongodb.net/test', {
+  //auth: { username: "dmaxwell", Password: "ZiscohFcN6FkC5zb" },
+  dbName: "aramarkDB",
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}, function(err, db) {
+  if (err) {
+    console.log('mongo db error  ', err);
+  }
+});
+
+// mongoose.connect('mongodb+srv://cluster0.bhz6k.mongodb.net/test/aramarkDB', {
+//   auth: {
+//     user: 'dmaxwell',
+//     password: 'ZiscohFcN6FkC5zb'
+//   },
+//    useNewUrlParser: true
+// })
+
+
+// Required aplication specific custom router module
+//var winthemeRouter = require('./src/routes/winthemeRouter');
+
+// Use middlewares to set view engine and post json data to the server
 app.use(express.static('public'));
-app.use(cors({origin: '*'}));
-//app.use(cors());
+app.use(cors());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
-//app.use(body-parser.json());
 
-const PORT = process.env.PORT || 3000
 
-app.use(express.json())
-//app.use(header_middleware)
-
-//app.use("/api/user", userRoutes);
 
 require('./src/routes/masterdataRoute.js')(app);
 
 require('./src/routes/winthemeRoute.js')(app);
 
-app.get('/test', (req, res) => {
-    res.send('Hello World!')
-  })
+
+// Start the server
+app.listen(port, function() {
+    console.log('Server is running on Port: ', port);
+  //  console.log(request.headers.host);
+    //console.log(window.location.hostname);
+});
+
+app.use(express.static(path.join(__dirname, '/client/build')));
+
+app.get('*', function(req, res){
+     res.sendFile(path.join(__dirname,'/client/build/index.html'));
+     console.log(req.headers.host);
+     console.log(__dirname);
+
+});
 
 
 
+app.listen(3000);
 
-// app.use((req,res,next)=>{
-//     res.sendFile(path.join(__dirname,"angular","index.html"))
-// });
-
-
-
-// app.use(express.static(path.join(__dirname, '/client/build')));
-
-// app.get('*', function(req, res){
-//      res.sendFile(path.join(__dirname,'/client/build/index.html'));
-//      console.log(req.headers.host);
-//      console.log(__dirname);
-
-// });
-
-app.listen(PORT, (req,res) => {
-    
-    console.log(`app is listening to PORT ${PORT}`)
-})
-//app.listen(PORT);
