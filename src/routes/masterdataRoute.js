7d0c36d0c3dede3f3c81b7960e6f1217ffc3741f
manoj <manojkumar.choubey@primeroedge.com>
2022-07-13 20:01:55 +0530

    commentted controller path

--- src/routes/masterdataRoute.js
+++ src/routes/masterdataRoute.js
@@ -1,27 +1,27 @@
 module.exports = (app) => {
-     const masterdata = require('../controllers/masterdata.js');
-    //const clientData = require('../controllers/clientdetail.js');
+    //  const masterdata = require('../controllers/masterdata.js');
+    // //const clientData = require('../controllers/clientdetail.js');
 
     
-    // app.post('/add/post', clientData.create);
+    // // app.post('/add/post', clientData.create);
 
-    // app.get('/allclient', clientData.findAll);
+    // // app.get('/allclient', clientData.findAll);
 
-    // app.get('/clientbyid/:id', clientData.findOne);
+    // // app.get('/clientbyid/:id', clientData.findOne);
 
-    // app.get('/clientbyemail/:email', clientData.findByEmail);
+    // // app.get('/clientbyemail/:email', clientData.findByEmail);
 
-    app.get('/getmasterdata', masterdata.findAll);
+    // app.get('/getmasterdata', masterdata.findAll);
 
-    app.get('/contracttypelist', masterdata.findContractTypeList);
+    // app.get('/contracttypelist', masterdata.findContractTypeList);
 
-    app.get('/industrytype', masterdata.findTndustryType);
+    // app.get('/industrytype', masterdata.findTndustryType);
 
-    app.get('/wintheme', masterdata.findwinTheme);
+    // app.get('/wintheme', masterdata.findwinTheme);
 
-    app.get('/customisableconvenienceoption', masterdata.findCustomisableConvenienceOption);
+    // app.get('/customisableconvenienceoption', masterdata.findCustomisableConvenienceOption);
     
-    // app.put('//update/:id',  clientData.update);
+    // // app.put('//update/:id',  clientData.update);
 
-    // app.delete('/delete/:id', clientData.delete);
+    // // app.delete('/delete/:id', clientData.delete);
 }
