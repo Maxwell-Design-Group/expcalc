aec6e61d265e17142b092e4d2581c01fa02f6f60
manoj <manojkumar.choubey@primeroedge.com>
2022-07-13 19:50:57 +0530

    checking controller

--- src/controllers/masterdata.js
+++ src/controllers/masterdata.js
@@ -1,207 +1,207 @@
-const contractTypeList = require('../models/contracttypelist.js');
-const industryType = require('../models/industrytype.js');
-const stationList = require('../models/stationlist.js');
-const wtProduct = require('../models/wtproduct.js');
-const winTheme = require('../models/wintheme.js');
-const customisableConvenienceOption = require('../models/customisableconvenienceoption.js');
-const supportingFeature = require('../models/supportingfeature.js');
-const digitalSignage = require('../models/digitalsignage.js');
-const cateringDetail = require('../models/cateringdetail.js');
+// const contractTypeList = require('../models/contracttypelist.js');
+// const industryType = require('../models/industrytype.js');
+// const stationList = require('../models/stationlist.js');
+// const wtProduct = require('../models/wtproduct.js');
+// const winTheme = require('../models/wintheme.js');
+// const customisableConvenienceOption = require('../models/customisableconvenienceoption.js');
+// const supportingFeature = require('../models/supportingfeature.js');
+// const digitalSignage = require('../models/digitalsignage.js');
+// const cateringDetail = require('../models/cateringdetail.js');
 
 
-exports.findAll = (req, res) => {
+// exports.findAll = (req, res) => {
 
-    var gcontracttypelist = [];
-    var gindustrytype=[];
-    var gstationList=[];
-    var gwtproduct=[];
+//     var gcontracttypelist = [];
+//     var gindustrytype=[];
+//     var gstationList=[];
+//     var gwtproduct=[];
 
-    var gsupportingfeature=[];
-    var gdigitalsignage=[];
-    var gcateringdetail=[];
-    var gwth=[];
-    var gccoption =[];
+//     var gsupportingfeature=[];
+//     var gdigitalsignage=[];
+//     var gcateringdetail=[];
+//     var gwth=[];
+//     var gccoption =[];
     
 
-	contractTypeList.find()
-    .then(contracttypelist => {
+// 	contractTypeList.find()
+//     .then(contracttypelist => {
        
-       gcontracttypelist=contracttypelist;
-    }).catch(err => {
-        res.status(500).send({
-            message: err.message || "Something went wrong."
-        });
-    });
+//        gcontracttypelist=contracttypelist;
+//     }).catch(err => {
+//         res.status(500).send({
+//             message: err.message || "Something went wrong."
+//         });
+//     });
 
-    //-----------------------------------------
-    industryType.find()
-    .then(industrytype => {
+//     //-----------------------------------------
+//     industryType.find()
+//     .then(industrytype => {
        
-        gindustrytype = industrytype;
+//         gindustrytype = industrytype;
         
-    }).catch(err => {
-        res.status(500).send({
-            message: err.message || "Something went wrong."
-        });
-    });
+//     }).catch(err => {
+//         res.status(500).send({
+//             message: err.message || "Something went wrong."
+//         });
+//     });
 
-    //-----------------------------------------
-    stationList.find()
-    .then(stationList => {
+//     //-----------------------------------------
+//     stationList.find()
+//     .then(stationList => {
        
-        gstationList = stationList;
+//         gstationList = stationList;
        
-    }).catch(err => {
-        res.status(500).send({
-            message: err.message || "Something went wrong."
-        });
-    });
+//     }).catch(err => {
+//         res.status(500).send({
+//             message: err.message || "Something went wrong."
+//         });
+//     });
 
-    //-----------------------------------------
-    wtProduct.find()
-    .then(wtproduct => {
+//     //-----------------------------------------
+//     wtProduct.find()
+//     .then(wtproduct => {
        
-        gwtproduct = wtproduct;
+//         gwtproduct = wtproduct;
        
-    }).catch(err => {
-        res.status(500).send({
-            message: err.message || "Something went wrong."
-        });
-    });
+//     }).catch(err => {
+//         res.status(500).send({
+//             message: err.message || "Something went wrong."
+//         });
+//     });
     
 
-    //-----------------------------------------
-    winTheme.find()
-    .then(wintheme => {
+//     //-----------------------------------------
+//     winTheme.find()
+//     .then(wintheme => {
        
-        gwth = wintheme;
+//         gwth = wintheme;
        
        
-    }).catch(err => {
-        res.status(500).send({
-            message: err.message || "Something went wrong."
-        });
-    });
+//     }).catch(err => {
+//         res.status(500).send({
+//             message: err.message || "Something went wrong."
+//         });
+//     });
 
-    //-----------------------------------------
-    customisableConvenienceOption.find()
-    .then(c => {
+//     //-----------------------------------------
+//     customisableConvenienceOption.find()
+//     .then(c => {
        
-        gccoption = c;       
+//         gccoption = c;       
 
         
-    }).catch(err => {
-        res.status(500).send({
-            message: err.message || "Something went wrong."
-        });
-    });
+//     }).catch(err => {
+//         res.status(500).send({
+//             message: err.message || "Something went wrong."
+//         });
+//     });
 
-    //-----------------------------------------
-    supportingFeature.find()
-    .then(supportingfeature => {
+//     //-----------------------------------------
+//     supportingFeature.find()
+//     .then(supportingfeature => {
        
-        gsupportingfeature = supportingfeature;
+//         gsupportingfeature = supportingfeature;
        
-    }).catch(err => {
-        res.status(500).send({
-            message: err.message || "Something went wrong."
-        });
-    });
+//     }).catch(err => {
+//         res.status(500).send({
+//             message: err.message || "Something went wrong."
+//         });
+//     });
 
-    //-----------------------------------------
-    digitalSignage.find()
-    .then(digitalsignage => {
+//     //-----------------------------------------
+//     digitalSignage.find()
+//     .then(digitalsignage => {
        
-        gdigitalsignage = digitalsignage;
+//         gdigitalsignage = digitalsignage;
         
-    }).catch(err => {
-        res.status(500).send({
-            message: err.message || "Something went wrong."
-        });
-    });
+//     }).catch(err => {
+//         res.status(500).send({
+//             message: err.message || "Something went wrong."
+//         });
+//     });
 
 
-    //-----------------------------------------
-    cateringDetail.find()
-    .then(cateringdetail => {
+//     //-----------------------------------------
+//     cateringDetail.find()
+//     .then(cateringdetail => {
        
 
-        gcateringdetail = cateringdetail;
-        console.log(gwth);
-        return res.status(200).send({
-            success: true,        
-            contracttypelist:gcontracttypelist,
-            industrytype: gindustrytype,
-            stationdata:gstationList,
-            wtproduct: gwtproduct, 
+//         gcateringdetail = cateringdetail;
+//         console.log(gwth);
+//         return res.status(200).send({
+//             success: true,        
+//             contracttypelist:gcontracttypelist,
+//             industrytype: gindustrytype,
+//             stationdata:gstationList,
+//             wtproduct: gwtproduct, 
 
-            supportingfeature:gsupportingfeature,
-            digitalsignage:gdigitalsignage,
-            cateringdetail:gcateringdetail,
+//             supportingfeature:gsupportingfeature,
+//             digitalsignage:gdigitalsignage,
+//             cateringdetail:gcateringdetail,
 
-            ccoption: gwth,
-            wintheme:gccoption
+//             ccoption: gwth,
+//             wintheme:gccoption
             
-          });
-    }).catch(err => {
-        res.status(500).send({
-            message: err.message || "Something went wrong."
-        });
-    });
+//           });
+//     }).catch(err => {
+//         res.status(500).send({
+//             message: err.message || "Something went wrong."
+//         });
+//     });
 
     
-};
+// };
 
-exports.findContractTypeList = (req, res) => {
+// exports.findContractTypeList = (req, res) => {
 
-    contractTypeList.find()
-    .then(contracttypelist => {
-        res.send(contracttypelist);
+//     contractTypeList.find()
+//     .then(contracttypelist => {
+//         res.send(contracttypelist);
        
-    }).catch(err => {
-        res.status(500).send({
-            message: err.message || "Something went wrong."
-        });
-    });
-}
+//     }).catch(err => {
+//         res.status(500).send({
+//             message: err.message || "Something went wrong."
+//         });
+//     });
+// }
 
 
-exports.findTndustryType = (req, res) => {
-    industryType.find()
-    .then(industrytype => {
-        res.send(industrytype);
+// exports.findTndustryType = (req, res) => {
+//     industryType.find()
+//     .then(industrytype => {
+//         res.send(industrytype);
         
         
-    }).catch(err => {
-        res.status(500).send({
-            message: err.message || "Something went wrong."
-        });
-    });
-}
+//     }).catch(err => {
+//         res.status(500).send({
+//             message: err.message || "Something went wrong."
+//         });
+//     });
+// }
 
 
-exports.findwinTheme = (req, res) => {
-    winTheme.find()
-    .then(wintheme => {
-        res.send(wintheme);       
+// exports.findwinTheme = (req, res) => {
+//     winTheme.find()
+//     .then(wintheme => {
+//         res.send(wintheme);       
        
-    }).catch(err => {
-        res.status(500).send({
-            message: err.message || "Something went wrong."
-        });
-    });
-}
+//     }).catch(err => {
+//         res.status(500).send({
+//             message: err.message || "Something went wrong."
+//         });
+//     });
+// }
 
-exports.findCustomisableConvenienceOption = (req, res) => {
-    customisableConvenienceOption.find()
-    .then(c => {
+// exports.findCustomisableConvenienceOption = (req, res) => {
+//     customisableConvenienceOption.find()
+//     .then(c => {
        
               
-        res.send(c); 
+//         res.send(c); 
         
-    }).catch(err => {
-        res.status(500).send({
-            message: err.message || "Something went wrong."
-        });
-    });
-}
+//     }).catch(err => {
+//         res.status(500).send({
+//             message: err.message || "Something went wrong."
+//         });
+//     });
+// }
