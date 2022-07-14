36e30c86e2c646701e2c7ce23ddffa4c13187132
manoj <manojkumar.choubey@primeroedge.com>
2022-07-14 05:49:26 +0530

    added both route

--- src/routes/masterdataRoute.js
+++ src/routes/masterdataRoute.js
@@ -1,15 +1,15 @@
 module.exports = (app) => {
     const common = require('../controllers/common.js');
-    //const clientData = require('../controllers/clientdetail.js');
+    const clientData = require('../controllers/clientdetail.js');
 
     
-    // app.post('/client', clientData.create);
+    app.post('/client', clientData.create);
 
-    // app.get('/client', clientData.find);
+    app.get('/client', clientData.find);
 
-    // app.delete('/client', clientData.delete);
+    app.delete('/client', clientData.delete);
 
-    // app.put('/client/:id',  clientData.update);
+    app.put('/client/:id',  clientData.update);
 
 
     // app.get('/master', masterdata.findAll);
@@ -21,5 +21,15 @@ module.exports = (app) => {
     // app.get('/wintheme', masterdata.findwinTheme);
 
     // app.get('/ccoption', masterdata.findCustomisableConvenienceOption);
+
+    // app.get('/master', common.findAll);
+
+    // app.get('/contract', common.findContractTypeList);
+
+    // app.get('/industry', common.findTndustryType);
+
+    // app.get('/wintheme', common.findwinTheme);
+
+    // app.get('/ccoption', common.findCustomisableConvenienceOption);
     
    }
