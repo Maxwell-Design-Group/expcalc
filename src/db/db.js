842fbe9b935871aead3bb0d1cd28d182783e1c19
manoj <manojkumar.choubey@primeroedge.com>
2022-07-19 13:37:02 +0530

    Manoj : added envirnment file for access connection string

--- src/db/db.js
+++ src/db/db.js
@@ -2,12 +2,17 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 console.log("db connection strings ");
-//console.log(process.env.DB_CONNECTION);
-//console.log(process.env.DB_CONNECTION);
+console.log(process.env.DB_CONNECTION);
+console.log(process.env.DB_CONNECTIONLOCAL);
 
 mongoose.Promise = global.Promise;
+// // Connect MongoDB at cloude 
+// const url = 'mongodb+srv://dmaxwell:ZiscohFcN6FkC5zb@cluster0.bhz6k.mongodb.net/test';
+// // Connect MongoDB at default port 27017.
+// const url_local = 'mongodb://localhost:27017';//process.env.DB_CONNECTIONLOCAL
+
 // Connect MongoDB at cloude 
-const url = 'mongodb+srv://dmaxwell:ZiscohFcN6FkC5zb@cluster0.bhz6k.mongodb.net/test';
+const url = process.env.DB_CONNECTION;
 // Connect MongoDB at default port 27017.
 const url_local = process.env.DB_CONNECTIONLOCAL
     
